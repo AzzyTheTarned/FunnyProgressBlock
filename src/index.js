404bf1540990a00fc8604001f78b16088875da8b
AzzyTheTarned <azzythetarned@gmail.com>
2024-06-08 01:39:58 +1000

    chore: Add FunnyGame component for a funny block filling game

--- src/index.js
+++ src/index.js
@@ -5,9 +5,44 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function FunnyGame() {
+  const [userValue, setUserValue] = React.useState(50);
+  const [isPending, setIsPending] = React.useState(false);
+  const onChange = (e) => {
+    const value = e.target.value;
+    if (value >= 0 && value <= 100) {
+      setIsPending(false);
+      setUserValue(value);
+    }
+    if (value == '') {
+      setIsPending(true);
+    }
+  }
+  return (
+    <>
+      <h3>Смешная игра "Заполни блок"</h3>
+      <label>
+        Процент заполнения:
+        <input type="number" min="0" max="100" value={userValue} onChange={onChange}/>
+      </label>
+      <FunnyBlock progress={userValue} pending={isPending}/>
+    </>
+  )
+}
+
+function FunnyBlock(props) {
+  return (
+    <div class="block">
+      <div class="progress" style={{width: `${props.pending ? '100' : props.progress}%`, background: `${props.pending ? 'red' : '#4CAF50'}`}}></div>
+    </div>
+  )
+}
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <FunnyGame />
   </React.StrictMode>
 );
 
